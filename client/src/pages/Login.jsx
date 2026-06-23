3a0a4935285141bb2221bada61b24db9cf5b6e38
abhijeet-raghuvanshi <abhijeetraghuvanshi9842abhi@gmail.com>
2026-06-24 01:45:09 +0530

    react initalization

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import api from "../api/axios.js";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await api.post("/auth/login", {
+        email,
+        password
+      });
+
+      localStorage.setItem("token", res.data.token);
+
+      alert("Login successful");
+    } catch (err) {
+      console.log(err);
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        placeholder="password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
