c728c72fe0a89994c62dddebe3d6a63236b4a968
abhijeet-raghuvanshi <abhijeetraghuvanshi9842abhi@gmail.com>
2026-06-24 13:16:54 +0530

    added keys to env

--- server/src/config/keys.js
+++ server/src/config/keys.js
@@ -1,18 +1,4 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const privateKey = fs.readFileSync(
-  path.resolve(__dirname, "../../keys/private.key"),
-  "utf8"
-);
-
-const publicKey = fs.readFileSync(
-  path.resolve(__dirname, "../../keys/public.key"),
-  "utf8"
-);
+const privateKey = process.env.JWT_PRIVATE_KEY;
+const publicKey = process.env.JWT_PUBLIC_KEY;
 
 export { privateKey, publicKey };
